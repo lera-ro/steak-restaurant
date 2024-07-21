d6d307edc7e630fba63a0f6c33778dd26d1d09ca
lera-ro <lera.rodoman@yandex.ru>
2024-07-21 15:24:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+gsap.from(".pic", {ease: "power3.in", y: "300px", duration: 2, opacity: 0})
+gsap.from(".btn", {ease: "power3.in", y: "300px", delay: 1, duration: 2, opacity: 0})
+gsap.from(".logo", {ease: "power3.in", duration: 2, opacity: 0, stagger: 1})
+gsap.from(".link", {ease: "power3.in", delay: 1, duration: 2, opacity: 0, stagger: 1})
